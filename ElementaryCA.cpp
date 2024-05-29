70c8c2ae7802265d8efa5b7b1cbbbb4e8e5f7068
wooJake <167344799+wooJake@users.noreply.github.com>
2024-05-28 18:24:06 -0700

    Update ElementaryCA.cpp

--- ElementaryCA.cpp
+++ ElementaryCA.cpp
@@ -1,39 +1,145 @@
-#include "LList.h"
+#include "ElementaryCA.h"
 
-int LList::ReturnRule(){
+bool ElementaryCA::BinaryRule(int left, int middle, int right, int index) {
 
-	return rule;
+	//Checking state of (from position) top left, top center, and top right to see what rule must be invoked.
+	return left == list->ReturnBinaryRule(0) &&
+		middle == list->ReturnBinaryRule(1) &&
+		right == list->ReturnBinaryRule(2);
 }
-int LList::ReturnBinaryRule(int index){
+int ElementaryCA::Rules(int left, int middle, int right) {
 
-	return binaryRule[index];
+	//Looping through all 8 rules.
+	for (int i = 0; i < 8; ++i) {
+
+		list->GetNext();
+
+
+		//If the binarRule is true (1), set to 1 otherwise set to false (0).
+		if (BinaryRule(left, middle, right, i)) {
+
+			return list->ReturnRule();
+		}
+	}
+	return 0;
 }
-LList::LList(){
 
-	rule = -1;
+void ElementaryCA::GenList() {
 
-	binaryRule[0] = -1;
-	binaryRule[1] = -1;
-	binaryRule[2] = -1;
+	unsigned int magicNums[3] = { (magicNum) % 2, (magicNum >> 1) % 2, (magicNum >> 2) % 2 };
 
-	head = nullptr;
+    list->head = new LList(ruleNum % 2, magicNums);
+
+	magicNum = magicNum >> 3;
+	ruleNum = ruleNum >> 1;
+
+	LList* currNod = list->head;
+	
+	for (int i = 1; i < 8; ++i) {
+
+		magicNums[0] = (magicNum) % 2;
+		magicNums[1] = (magicNum >> 1) % 2;
+		magicNums[2] = (magicNum >> 2) % 2;
+
+		currNod->InsertAfter(new LList(ruleNum % 2, magicNums));
+
+		magicNum = magicNum >> 3;
+		ruleNum = ruleNum >> 1;
+		currNod->InsertAfter(list->GetNext());
+	}
+	list = currNod;
 }
-LList::LList(unsigned int rule_in, unsigned int bin_in[3]) {
 
-	rule = rule_in;
+void ElementaryCA::Game() {
 
-	binaryRule[0] = bin_in[0];
-	binaryRule[1] = bin_in[1];
-	binaryRule[2] = bin_in[2];
+	list = new LList();
 
-	head = nullptr;
+	std::ofstream outFS;
 
-	//std::cout << "rule: " << rule << "\n";
-	//std::cout << "binaryRule[0]: " << binaryRule[0] << "\n";
-	//std::cout << "binaryRule[1]: " << binaryRule[1] << "\n";
-	//std::cout << "binaryRule[2]: " << binaryRule[2] << "\n";
+	std::string fileName = "WolframCAout.txt";
+
+	outFS.open(fileName);
+
+	unsigned int whileSize = BOARD_SIZE * 0x18;
+
+	//Setting up board.
+	for (unsigned int i = 0; i < BOARD_SIZE; ++i) {
+
+		//Setting random number between 0 and 1 (this is all that is needed since this is all in binary).
+		board[i] = rand() % 2;
+		board2[i] = board[i];
+	}
+
+	while (counter <= whileSize) {
+
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+
+			//Making sure there is no error when on the leftmost and rightmost sides.
+			int left = i == 0 ? board[BOARD_SIZE - 1] : board[i - 1];
+			int middle = board[i];
+			int right = i == BOARD_SIZE - 1 ? board[0] : board[i + 1];
+
+			//Checking rules and storing the result in board2 so there is no conflict between readings.
+			board2[i] = Rules(left, middle, right);
+		}
+
+		//Looping through the board again to display and set the first board equal to the second board.
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+
+			//Setting board 1 to board 2.
+			board[i] = board2[i];
+
+			textLine.push_back(board[i]);
+
+			//If board at i is 0 then display nothing, otherwise set it to a visible character.
+			if (board[i] == 0) {
+
+				std::cout << ' ';
+			}
+			else {
+
+				std::cout << 'O';
+			}
+		}
+		//At the end of a line go to next line visibly.
+		std::cout << "\n";
+
+		++counter;
+	}
+
+	for (int i = 0; i <= whileSize; ++i) {
+
+		if (i % BOARD_SIZE == 0) {
+
+			outFS << std::endl;
+			continue;
+		}
+
+		if (textLine.at(i) == 0) {
+
+			outFS << ' ';
+			continue;
+		}
+		outFS << 'O';
+	}
 }
-LList::~LList(){
+ElementaryCA::ElementaryCA() {
 
+	list = new LList();
 
+	magicNum = 0xFAC688;
+	ruleNum = 0b00011110;
+
+	counter = 0;
+
+	board = new unsigned int[BOARD_SIZE];
+	board2 = new unsigned int[BOARD_SIZE];
+
+	GenList();
+}
+ElementaryCA::~ElementaryCA() {
+
+	//Deleting boards.
+	delete[] board;
+	delete[] board2;
 }
